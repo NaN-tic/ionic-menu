e4a24d42b79db45fb30deb1eb435bd63bb35fc79
Raimon Esteve <raimon@nan-tic.com>
2017-11-08 14:24:25 +0100

    Code style

--- menu.ts
+++ menu.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-    selector: 'page-menu',
-    templateUrl: 'menu.html',
-
+  selector: 'page-menu',
+  templateUrl: 'menu.html',
 })
+
 /**
  * Menu page.
  * This class display the different menu options and go to their
@@ -16,25 +16,24 @@ import { NavController } from 'ionic-angular';
  * DO NOT MODIFY THIS CLASS, EXTEND IT INSTEAD
  */
 export class MenuPage {
-    /**
-     * Array contaning the menus
-     * @type {Array}
-     */
-    menu: Array<{name: string, page: any, params?: {}, disabled?: boolean}>;
-    title: string;
+  /**
+   * Array contaning the menus
+   * @type {Array}
+   */
+  menu: Array<{name: string, page: any, params?: {}, disabled?: boolean}>;
+  title: string;
 
-    constructor(public navCtrl: NavController) {
-    }
+  constructor(public navCtrl: NavController) {
+  }
 
-    /**
-     * Listener for the menu buttons, when a button is pressed in the menu
-     * it calls this method.
-     * @param {Object} $event Event that happend
-     * @param {Object} entry  Entry information (name and page to go)
-     */
-    entryTapped($event, entry) {
-        console.log("Tapped entry", entry, entry.params);
-        this.navCtrl.push(entry.page, {params: entry.params});
-    }
-
+  /**
+   * Listener for the menu buttons, when a button is pressed in the menu
+   * it calls this method.
+   * @param {Object} $event Event that happend
+   * @param {Object} entry  Entry information (name and page to go)
+   */
+  entryTapped($event, entry) {
+    console.log("Tapped entry", entry, entry.params);
+    this.navCtrl.push(entry.page, {params: entry.params});
+  }
 }
